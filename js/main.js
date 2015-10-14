ceb920263f2521b9a3f96d311e56f9ba242b30e4
Justin Punzalan <justpunz@Live.com>
2015-10-13 23:01:15 -0700

    Removed js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.href);
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
